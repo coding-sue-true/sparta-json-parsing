902f8270f890afd6c2a0813d2c2724c1cbec805e
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2017-12-13 11:43:04 +0000

    all test ran

--- json_parsing_tests/json_class_parse.rb
+++ json_parsing_tests/json_class_parse.rb
@@ -8,5 +8,23 @@ class ParseJson
     @json_file = JSON.parse(File.read(json_file))
   end
 
+  def get_base_value
+    @json_file['base']
+  end
 
+  def get_rates_values
+    @json_file['rates'].values
+  end
+
+  def get_rates_keys
+    @json_file['rates'].keys
+  end
+
+  def get_rates
+    @json_file['rates']
+  end
+
+  def get_date
+    @json_file['date']
+  end
 end
